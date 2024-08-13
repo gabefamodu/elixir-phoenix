576c771e7142203c2a6d4572c2f6a9216b533d8c
gabrielfamodu <gabefamodu13@gmail.com>
2024-08-13 15:01:25 -0600

    added new user controller

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,19 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    # IO.puts("Users fiunction hit! ")
+    # Created map
+    users = [
+      %{id: 1, name: "Gabe", email: "test@email.com"},
+      %{id: 2, name: "Jen", email: "Jen@email.com"}
+    ]
+    # render(conn, :users, users: users, layout: false)
+
+    # Created json data
+    json(conn, %{users: users})
+  end
+
+
 end
